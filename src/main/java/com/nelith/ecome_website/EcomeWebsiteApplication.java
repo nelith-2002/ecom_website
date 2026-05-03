5b2098020c3cb9fa1ee875f58c78900bad35bf4c
Nelith Nethsanda <nelithnethsanda2002@gmail.com>
2026-05-03 22:30:23 +0530

    feat: project setup

---
+++ src/main/java/com/nelith/ecome_website/EcomeWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.nelith.ecome_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomeWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomeWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nelith/ecome_website/EcomeWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nelith.ecome_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomeWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
